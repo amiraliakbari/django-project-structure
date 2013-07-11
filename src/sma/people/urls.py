44a3aa5315b121d6ff95f9b72f02e3bdf5e18ce2
AmirAli Akbari <akbari@arsh.co>
2013-07-11 17:13:51 +0430

    remove sample app

--- src/sma/people/urls.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('sma.people.views',
-    url(r'^(?:dashboard)$', 'dashboard', name='people/dashboard'),
-)
--- src/sma/people/views.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-
-def dashboard(request):
-    return render_to_response('people/dashboard.html', context_instance=RequestContext(request))
